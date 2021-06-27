21ed663cd03a4a94a7c2eff2fa4312a773b191bf
Berkay GÜRE <bgure@hotmail.com.tr>
2021-06-27 12:16:30 +0300

    added swagger

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -6,12 +6,15 @@ const io = require('socket.io')(http);
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 require('./config/index.js')(app, mongoose, express);
 
 // Register routes
 app.use(routes);
 app.use(errorHandler);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 http.listen(process.env.PORT || 8080, function () {
   console.log('listening on :' + process.env.PORT);
